94801a2edbc38299bf02be38ca5640b6a94609fb
joshatdia <joshua@dialogicaai.com>
2025-11-11 12:09:07 -0800

    changed the name to match

---
+++ target/debug/build/ggml-rs-e0de4135ddc57ce1/build_script_build-e0de4135ddc57ce1.d
@@ -0,0 +1,5 @@
+C:\Users\JoshuaGoodman\Documents\GitHub\ggml-rs\target\debug\build\ggml-rs-e0de4135ddc57ce1\build_script_build-e0de4135ddc57ce1.d: build.rs
+
+C:\Users\JoshuaGoodman\Documents\GitHub\ggml-rs\target\debug\build\ggml-rs-e0de4135ddc57ce1\build_script_build-e0de4135ddc57ce1.exe: build.rs
+
+build.rs:
